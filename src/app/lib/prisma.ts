fcef8c3a3eb4f77cfb369b66c313d0abfa6f7ed0
David Zeff <dpzeff@gmail.com>
2025-08-19 08:45:08 +0300

    Update prisma.ts

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,13 +1,1 @@
-import { PrismaClient } from "@/generated/prisma";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-export const prisma =
-  global.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
